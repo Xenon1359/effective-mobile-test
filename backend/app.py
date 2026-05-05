450428d559eebc136335ae8fb99d6edbe16fbf39
Xenon1359 <dmitry.kolobov9@gmail.com>
2026-05-05 14:27:40 +0000

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello from Effective Mobile!")
+
+    def log_message(self, format, *args):
+        return
+
+
+server = HTTPServer(("0.0.0.0", 8080), Handler)
+
+print("Backend started on port 8080")
+
+server.serve_forever()
